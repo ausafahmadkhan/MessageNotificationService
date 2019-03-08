5967fa931a16ed748a3b58028429a6f168e23753
ausafkhan <khan.aakhan.ausaf7@gmail.com>
2019-03-09 03:49:56 +0530

    modified App.java

--- App/src/main/java/com/Message/App/App.java
+++ App/src/main/java/com/Message/App/App.java
@@ -1,16 +1,9 @@
 package com.Message.App;
 
 
-import com.Message.Clients.ClientA.ClientAService.ClientAService;
-import com.Message.Clients.ClientA.ClientAValidator.ClientAValidator;
-import com.Message.Clients.ClientB.ClientBService.ClientBService;
-import com.Message.Clients.ClientB.ClientBValidator.ClientBValidator;
-import com.Message.Clients.ClientC.ClientCService.ClientCService;
-import com.Message.Clients.ClientC.ClientCValidator.ClientCValidator;
-import com.Message.Contracts.MessageResponse.NotificationError;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class App
@@ -19,5 +12,5 @@ public class App
     {
         SpringApplication.run(App.class, args);
     }
-    
+
 }
